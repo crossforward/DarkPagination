0cf0dd56d756f15c516de718031d18af81b4650b
David Smith <davidsmith@Lauren-Smiths-MacBook-Air.local>
2012-05-13 13:53:02 -0400

    Initial Commit

---
+++ DarkPagination/DTSAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DTSAppDelegate.h
+//  DarkPagination
+//
+//  Created by David Smith on 5/13/12.
+//  Copyright (c) 2012 Cross Forward Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DTSViewController;
+
+@interface DTSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DTSViewController *viewController;
+
+@end
---
+++ DarkPagination/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DarkPagination
+//
+//  Created by David Smith on 5/13/12.
+//  Copyright (c) 2012 Cross Forward Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTSAppDelegate class]));
+    }
+}
